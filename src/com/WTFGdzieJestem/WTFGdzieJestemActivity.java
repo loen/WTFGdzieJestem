3c10bb912821e1e2bac1c5fb4e650aedd4ae5e20
Sebastian Mikucki <gozilla3000@gmail.com>
2012-02-18 14:47:39 +0100

    Update

--- src/com/WTFGdzieJestem/WTFGdzieJestemActivity.java
+++ src/com/WTFGdzieJestem/WTFGdzieJestemActivity.java
@@ -20,9 +20,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import com.WTFGdzieJestem.util.client.GoogleMapsClient;
 
-public class WTFGdzieJestemActivity extends Activity implements
-		LocationListener {
+public class WTFGdzieJestemActivity extends Activity{
 
 	private static String PREFS_NAME = "WTF";
 	private static String LONGITUDE = "Longitude";
@@ -54,11 +54,16 @@ public class WTFGdzieJestemActivity extends Activity implements
 		homeLocationButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				initializeLocationProvider();
-
+				initializeLocationProvider(new HomeLocationListener());
 			}
 		});
         navButton = (Button)findViewById(R.id.goHomeButton);
+        navButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initializeLocationProvider(new GoHomeListener());
+            }
+        });
 		locationText = (EditText)findViewById(R.id.locationText);
 		locationObject = getLocation();
 		locationText.setText(locationObject.getLocationName());
@@ -89,38 +94,6 @@ public class WTFGdzieJestemActivity extends Activity implements
 				
 			}
 		});
-	}
-
-	@Override
-	public void onLocationChanged(Location location) {
-		if (location != null) {
-			locationManager.removeUpdates(this);
-			double longitude = location.getLongitude();
-			double lattitude = location.getLatitude();
-
-			String locationName = getHumanReadableLocation(longitude, lattitude);
-			saveLocation(longitude,lattitude,locationName);
-			locationText.setText(locationName);
-		}
-
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-		Log.d(TAG, "Provider Disabled");
-
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-		Log.d(TAG, "Provider Enabled");
-
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		Log.d(TAG, "Status Changed");
-
 	}
 
 	private String getHumanReadableLocation(double longitude, double lattitude) {
@@ -168,14 +141,92 @@ public class WTFGdzieJestemActivity extends Activity implements
 		
 	}
 	
-	private void initializeLocationProvider() {
+	private void initializeLocationProvider(LocationListener locationListener) {
 		try{
 		locationManager = (LocationManager) getApplicationContext()
 				.getSystemService(Context.LOCATION_SERVICE);
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				60000, 0, this);
+				60000, 0, locationListener);
 		}catch (Exception e) {
 			Log.d(TAG, "error");
 		}
 	}
+
+    private class GoHomeListener implements LocationListener {
+
+
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                locationManager.removeUpdates(this);
+                SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+                String destLatitude = preferences.getString(LATTITUDE, "0");
+                String destLongitude = preferences.getString(LONGITUDE, "0");
+                double srcLatitude = location.getLatitude();
+                double srcLongitude = location.getLongitude();
+                setIntent(GoogleMapsClient.getGoogleMapsIntent(valueOf(destLatitude), valueOf(destLongitude), srcLatitude, srcLongitude));
+            }
+        }
+        
+        private double valueOf(String string){
+            return Double.valueOf(string);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d(TAG, "Provider Disabled");
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d(TAG, "Provider Enabled");
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d(TAG, "Status Changed");
+
+        }
+    }
+
+    private class HomeLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                locationManager.removeUpdates(this);
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                String locationName = getHumanReadableLocation(longitude, latitude);
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString(LONGITUDE, Double.toString(longitude));
+                editor.putString(LATTITUDE, Double.toString(latitude));
+                editor.putString(NAME, locationName);
+                editor.commit();
+                locationText.setText(locationName);
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d(TAG, "Provider Disabled");
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d(TAG, "Provider Enabled");
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d(TAG, "Status Changed");
+
+        }
+    }
+    
 }
