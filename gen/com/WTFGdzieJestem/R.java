fdeeaa431531a6fbebab0f5887cdcdf3e05c64ef
Sebastian Mikucki <gozilla3000@gmail.com>
2012-02-18 11:23:45 +0100

    Minor update

--- gen/com/WTFGdzieJestem/R.java
+++ gen/com/WTFGdzieJestem/R.java
@@ -18,6 +18,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int goHomeButtonHint=0x7f040006;
+        public static final int goHomeButtonName=0x7f040005;
         public static final int hello=0x7f040000;
+        public static final int homeLocationButtonHint=0x7f040003;
+        public static final int homeLocationButtonName=0x7f040002;
+        public static final int locationTextFieldValue=0x7f040004;
     }
 }
